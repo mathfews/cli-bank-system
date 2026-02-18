2384ff52298a9f1aa5dc2122139d78ec640070cb
matheus <matheusfcw1010@gmail.com>
2026-02-18 16:39:29 -0300

    created bank_ui fucntion

--- scripts/main.py
+++ scripts/main.py
@@ -3,20 +3,24 @@ from auth import Auth
 auth = Auth()
 def clean_terminal():
     os.system("cls" if os.name == "nt" else "clear")
+
+def bank_ui(active_user):
+    info = auth.show_info(active_user)
+    bank_ui = f"""╔════════════════╗
+║   BANKSTACK    ║
+╚════════════════╝
+Username :  {active_user}
+Current balance:  {info["current balance"]}
+Pending Incoming: {info["pending incoming"]}"""
+    print(bank_ui)
+
 def user_auth(type):
         while True:
             clean_terminal()
             username = input("Enter your username(Digit 0 to return): ")
             if username == "0":
-                clean_terminal()
-                break
-            while True:
-                if type == "login" and auth.user_exists(username) == False:
-                    clean_terminal()
-                    print(f"O usuário {username} não foi encontrado!")
-                    username = input("Enter your username(Digit 0 to return): ")
-                elif type == "login" and auth.user_exists(username) == True:
-                    break
+                 clean_terminal()
+                 break
             password = input("Enter your password: ")
             result = getattr(auth, type)(username,password)
             if type == "register" and result[0] == True:
@@ -26,14 +30,17 @@ def user_auth(type):
             if result[0]:
                 print(f"{result[1]} | Press enter in your account")
                 input("")
-                break
+                return username
             print(f"{result[1]} | Press enter to try again")
             input("")
 while True:
     print("Menu \n🔐1) Login\n📝2) Register")
     user_input = input("> ").strip().lower()
     if user_input == "1" or user_input == "login":
-        user_auth("login")
+        username = user_auth("login")
     if user_input == "2" or user_input == "register":
         user_auth("register")
-    clean_terminal()
+    while True:
+         clean_terminal()
+         bank_ui(username)
+         input("")
